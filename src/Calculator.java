dac215367412240293b99a5f193f1aedb2e6cf5d
Alexander Kozhin <hustler77@mail.ru>
2022-05-10 22:37:47 +0300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> divide = (x, y) -> y == 0 ? 0 : x / y; // ошибка при делении на "0". Если y=0 -возвращаем "0".
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+}
